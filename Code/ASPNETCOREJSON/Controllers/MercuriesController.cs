ffe712270e380347155c401b4f54a094d8272694
Istvan Marki <istv0050@edu.easj.dk>
2017-03-18 23:48:26 +0100

    bugs fixed commit

---
+++ Code/ASPNETCOREJSON/Controllers/MercuriesController.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ASPNETCOREJSON.Repository;
+using ASPNETCOREJSON.Models;
+
+namespace ASPNETCOREJSON.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Mercuries")]
+    public class MercuriesController : Controller
+    {
+        public IMercuriesRepository MercuriesRepo { get; set; }
+
+        public MercuriesController(IMercuriesRepository _repo)
+        {
+            MercuriesRepo = _repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Mercury> GetAll()
+        {
+            return MercuriesRepo.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetMercuries")]
+        public IActionResult GetById(string id)
+        {
+            var item = MercuriesRepo.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Create([FromBody] Mercury item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            MercuriesRepo.Add(item);
+            return CreatedAtRoute("GetMercuries", new {Controller = "Mercuries", id = item.Mercury_Id}, item);
+        }
+
+        [HttpPut]
+        public IActionResult Update(string id, [FromBody] Mercury item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            var mercuryObj = MercuriesRepo.Find(id);
+            if (mercuryObj == null)
+            {
+                return NotFound();
+            }
+            MercuriesRepo.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            MercuriesRepo.Remove(id);
+        }
+    }
+}
--- Code/Version2/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Version2.MvcApplication" Language="C#" %>
