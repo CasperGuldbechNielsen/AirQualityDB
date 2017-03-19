0b1dc1c8ce88523cbe7ca01305f67ab2d25493b9
Istvan Marki <istv0050@edu.easj.dk>
2017-03-19 13:45:30 +0100

    added CORS Policy for runtime too

--- Code/ASPNETCOREJSON/Startup.cs
+++ Code/ASPNETCOREJSON/Startup.cs
@@ -66,8 +66,11 @@ namespace ASPNETCOREJSON
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            app.UseCors("CorsPolicy");
 
             app.UseMvc();
         }
