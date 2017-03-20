48cd42c125395923d473a147a86724ada6febe8f
Istvan Marki <istv0050@edu.easj.dk>
2017-03-20 09:14:24 +0100

    added OzonesRepository of type IOzonesRepository

---
+++ Code/ASPNETCOREJSON/Repository/IOzonesRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCOREJSON.Models;
+
+namespace ASPNETCOREJSON.Repository
+{
+    interface IOzonesRepository
+    {
+        void Add(Ozone ozoneEntry);
+        IEnumerable<Ozone> GetAll();
+        Ozone Find(string id);
+        void Remove(string id);
+        void Update(Ozone ozoneEntry);
+
+    }
+}
