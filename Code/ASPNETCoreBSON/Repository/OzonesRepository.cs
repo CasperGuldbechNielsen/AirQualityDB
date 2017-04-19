7a3b88cf29491d0d8975765b41bef543c480822a
Istvan Marki <istv0050@edu.easj.dk>
2017-04-19 18:22:51 +0200

    ozone repo added

--- Code/ASPNETCoreBSON/Repository/OzonesRepository.cs
+++ Code/ASPNETCoreBSON/Repository/OzonesRepository.cs
@@ -3,35 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ASPNETCoreBSON.Model;
+using ASPNETCoreBSON.Contexts;
+using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
 namespace ASPNETCoreBSON.Repository
 {
     public class OzonesRepository: IOzonesRepository
     {
+        private readonly OzonesContext _context = null;
+
+        public OzonesRepository(IOptions<Settings> settings)
+        {
+            _context = new OzonesContext(settings);
+        }
+
         public async Task Add(Ozone ozoneEntry)
         {
-            throw new NotImplementedException();
+            await _context.Ozones.InsertOneAsync(ozoneEntry);
         }
 
         public async Task<IEnumerable<Ozone>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _context.Ozones.Find(_ => true).ToListAsync();
         }
 
         public async Task<Ozone> Find(string id)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Ozone>.Filter.Eq("_id", id);
+            return await _context.Ozones.Find(filter).FirstOrDefaultAsync();
         }
 
         public async Task<DeleteResult> Remove(string id)
         {
-            throw new NotImplementedException();
+            return await _context.Ozones.DeleteOneAsync(Builders<Ozone>.Filter.Eq("_id", id));
         }
 
         public async Task<UpdateResult> Update(Ozone ozoneEntry)
         {
-            throw new NotImplementedException();
+            //in reality this is the closest case study I would imagine:
+            //filter by the time it was taken
+            var filter = Builders<Ozone>.Filter.Eq(x => x.DateTimeStart, ozoneEntry.DateTimeStart);
+            //update or correct measurements
+            var update = Builders<Ozone>.Update.Set(x => x.Ozone1, ozoneEntry.Ozone1);
+
+            return await _context.Ozones.UpdateOneAsync(filter, update);
         }
     }
 }
