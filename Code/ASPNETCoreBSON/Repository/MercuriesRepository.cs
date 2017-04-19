c5a1707676df78400920373d7092debfc02d1b56
Istvan Marki <istv0050@edu.easj.dk>
2017-04-19 14:08:34 +0200

    finishing up Mercury repo

--- Code/ASPNETCoreBSON/Repository/MercuriesRepository.cs
+++ Code/ASPNETCoreBSON/Repository/MercuriesRepository.cs
@@ -3,35 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ASPNETCoreBSON.Model;
+using ASPNETCoreBSON.Contexts;
+using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 
 namespace ASPNETCoreBSON.Repository
 {
     public class MercuriesRepository : IMercuriesRepository
     {
+        private readonly MercuriesContext _context = null;
+
+        public MercuriesRepository(IOptions<Settings> settings)
+        {
+            _context = new MercuriesContext(settings);
+        }
+
         public async Task Add(Mercury mercuryEntry)
         {
-            throw new NotImplementedException();
+            await _context.Mercuries.InsertOneAsync(mercuryEntry);
         }
 
         public async Task<IEnumerable<Mercury>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _context.Mercuries.Find(_ => true).ToListAsync();
         }
 
         public async Task<Mercury> Find(string id)
         {
-            throw new NotImplementedException();
+            var filter = Builders<Mercury>.Filter.Eq("_id", id);
+            return await _context.Mercuries.Find(filter).FirstOrDefaultAsync();
         }
 
         public async Task<DeleteResult> Remove(string id)
         {
-            throw new NotImplementedException();
+            return await _context.Mercuries.DeleteOneAsync(Builders<Mercury>.Filter.Eq("_id", id));
         }
 
         public async Task<UpdateResult> Update(Mercury mercuryEntry)
         {
-            throw new NotImplementedException();
+            //in reality this is the closest case study I would imagine:
+            //filter by the time it was taken
+            var filter = Builders<Mercury>.Filter.Eq(x => x.DateTimeStart, mercuryEntry.DateTimeStart);
+            //update or correct the measurements
+            var update = Builders<Mercury>.Update.Set(x => x.Hg, mercuryEntry.Hg);
+
+            return await _context.Mercuries.UpdateOneAsync(filter, update);
         }
     }
 }
