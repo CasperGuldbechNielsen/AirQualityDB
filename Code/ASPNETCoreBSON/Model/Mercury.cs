00615055b178c9eb3d564a8fba43011628ebaa61
Istvan Marki <istv0050@edu.easj.dk>
2017-04-19 10:47:32 +0200

    creating POCO classes

---
+++ Code/ASPNETCoreBSON/Model/Mercury.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ASPNETCoreBSON.Model
+{
+    public class Mercury
+    {
+        [BsonId]
+        public int _id { get; set; }
+        public DateTime DateTimeStart { get; set; }
+        public double? Hg { get; set; }
+        public string unit { get; set; }
+    }
+}
---
+++ Code/ASPNETCoreBSON/Model/Ozone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ASPNETCoreBSON.Model
+{
+    public class Ozone
+    {
+        [BsonId]
+        public int Ozone_Id { get; set; }
+        public DateTime DateTimeStart { get; set; }
+        public double? Ozone1 { get; set; }
+        public string Unit { get; set; }
+    }
+}
