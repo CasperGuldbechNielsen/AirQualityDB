c4ea4b67cb054fdebb7e0654e2dd5f4759477afd
Istvan Marki <istv0050@edu.easj.dk>
2017-04-19 11:37:56 +0200

    adding contexts for IMongoDatabase

---
+++ Code/ASPNETCoreBSON/Contexts/MercuriesContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCoreBSON.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ASPNETCoreBSON.Contexts
+{
+    public class MercuriesContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public MercuriesContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+            
+        }
+
+        public IMongoCollection<Mercury> Mercuries
+        {
+            get { return _database.GetCollection<Mercury>("mercuryCollection"); }
+        }
+    }
+}
---
+++ Code/ASPNETCoreBSON/Contexts/OzonesContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCoreBSON.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace ASPNETCoreBSON.Contexts
+{
+    public class OzonesContext
+    {
+        private readonly IMongoDatabase _database = null;
+
+        public OzonesContext(IOptions<Settings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            if (client != null)
+                _database = client.GetDatabase(settings.Value.Database);
+        }
+
+        public IMongoCollection<Ozone> Ozones
+        {
+            get { return _database.GetCollection<Ozone>("ozoneCollection"); }
+        }
+    }
+}
